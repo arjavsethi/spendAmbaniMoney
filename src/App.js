32d71d46e64da11b4fa648e3e7f400c2faf7a23e
arjavsethi <arjavsethi07@gmail.com>
2021-10-13 23:49:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import Heading from './components/Heading';
+import './App.css';
+import Money from './components/Money';
+import ProductList from './components/ProductList';
+// import Product from './components/Product';
+import TotalState from './context/totalState';
+import Receipt from './components/Receipt';
+// import { StickyContainer, Sticky } from 'react-sticky';
+
+import About from './components/About';
+
+function App() {
+
+  return (
+    <>
+    
+    <TotalState>
+      
+   
+    <div className="App">
+     <Heading/>
+     <Money/>
+     </div>
+     <ProductList/>
+     <Receipt/>
+     <About/>
+     </TotalState>
+   
+    </>
+  );
+}
+
+export default App;
